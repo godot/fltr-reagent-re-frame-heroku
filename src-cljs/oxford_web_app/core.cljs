7dfe54a319672e3f1844499073a1131b39fb03d7
dariusz gorzeba <dgorzeba@gmail.com>
2015-04-19 17:21:05 +0200

    remove figwheel before deploying

--- src-cljs/oxford_web_app/core.cljs
+++ src-cljs/oxford_web_app/core.cljs
@@ -1,6 +1,6 @@
 (ns oxford-web-app.core
   (:require [clojure.string :as string]
-            [figwheel.client :as fw]
+            ;; [figwheel.client :as fw]
             [reagent.core :as reagent :refer [atom]]
             [reagent-forms.core :refer [bind-fields]]
             [oxford-web-app.articles.core :as articles]
@@ -102,24 +102,25 @@
   (reagent/render-component  [page] (.getElementById js/document "root")))
 
 
-(fw/start {
-  ;; configure a websocket url if you are using your own server
-  ;; :websocket-url "ws://localhost:3449/figwheel-ws"
+;; (fw/start {
+;;   ;; configure a websocket url if you are using your own server
+;;   ;; :websocket-url "ws://localhost:3449/figwheel-ws"
 
-  ;; optional callback
-  :on-jsload (fn [] (print "reloaded"))
+;;   ;; optional callback
+;;   :on-jsload (fn [] (print "reloaded"))
 
-  ;; The heads up display is enabled by default
-  ;; to disable it:
-  ;; :heads-up-display false
+;;   ;; The heads up display is enabled by default
+;;   ;; to disable it:
+;;   ;; :heads-up-display false
 
-  ;; when the compiler emits warnings figwheel
-  ;; blocks the loading of files.
-  ;; To disable this behavior:
-  ;; :load-warninged-code true
+;;   ;; when the compiler emits warnings figwheel
+;;   ;; blocks the loading of files.
+;;   ;; To disable this behavior:
+;;   ;; :load-warninged-code true
 
-  ;; if figwheel is watching more than one build
-  ;; it can be helpful to specify a build id for
-  ;; the client to focus on
-  ;; :build-id "example"
-})
+;;   ;; if figwheel is watching more than one build
+;;   ;; it can be helpful to specify a build id for
+;;   ;; the client to focus on
+;;   ;; :build-id "example"
+;;            }
+)
